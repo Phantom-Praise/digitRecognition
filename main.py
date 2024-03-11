5c14a096719f4664ba6cd884c3095386f008d6d2
Phantom-Praise <107717866+Phantom-Praise@users.noreply.github.com>
2024-03-11 14:12:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# DEPLOY WITH DJANGO
+#How to Build an AI Model with Tensorflow & Deploy with Django [ Step by Step Guide ]
+# https://youtu.be/MUYh8MCboqw?si=LEwrRf-x2bAFvCjo
+
+#Django and Tensorflow Integration
+
+# https://www.youtube.com/watch?v=Pc8WdnIdXZg&t=398s
